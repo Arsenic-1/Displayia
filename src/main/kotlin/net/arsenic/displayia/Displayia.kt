16a9e892b815dd7e8e7176662ebb683442d2a652
Arsenic <reeeeee197@gmail.com>
2025-06-07 16:47:03 -0400

    first commit

---
+++ src/main/kotlin/net/arsenic/displayia/Displayia.kt
@@ -0,0 +1,14 @@
+package net.arsenic.displayia
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Displayia : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
